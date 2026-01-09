70df537fcef73ed23eb1c19a5c9b1ba3ac565c2c
Mohamed Athiq <157815296+athiq14@users.noreply.github.com>
2026-01-09 10:20:14 +0530

    Add initial gallery page

---
+++ gallery.php
@@ -0,0 +1,24 @@
+<?php
+require_once __DIR__ . '/config.php';
+include 'header.php';
+?>
+
+<main id="page-content" style="padding-top:72px;">
+  <div class="container my-5">
+    <h2 class="text-center mb-4">Gallery</h2>
+
+    <div class="row">
+      <div class="col-md-4 mb-3">
+        <img src="uploads/banner.png" class="img-fluid rounded" alt="Gallery Image">
+      </div>
+      <div class="col-md-4 mb-3">
+        <img src="uploads/logo.png" class="img-fluid rounded" alt="Gallery Image">
+      </div>
+      <div class="col-md-4 mb-3">
+        <img src="uploads/banner.png" class="img-fluid rounded" alt="Gallery Image">
+      </div>
+    </div>
+  </div>
+</main>
+
+<?php include 'footer.php'; ?>
